3b89606f6344b6639c36aa73cf19a556021751fc
yasinadil <adilyasin205@gmail.com>
2023-03-02 04:03:59 +0500

    env var check

--- utils/connectMongo.ts
+++ utils/connectMongo.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 const connectMongo = async () => {
+   console.log(process.env.NEXT_PUBLIC_MONGO_URL!);
    
     return mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URL!);
       
