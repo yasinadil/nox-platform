3af8a2dba45c8dcbf63a5f225247f9df20883e63
yasinadil <adilyasin205@gmail.com>
2023-03-02 07:47:05 +0500

    new smart contract

--- Config.ts
+++ Config.ts
@@ -1,4 +1,4 @@
 export const noxTokenAddress = "0x385A96b35bdbC919D0Bd95670461CB2153813C71";
 export const WETH_TokenAddress = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6";
-export const noxNFTAddress = "0x7d89cB088aE1C1E154A707c1C075dbfb3fEB8444";
-export const noxPlatform = "0xa7399Fce0820d704661803a257682BD116500C62";
+export const noxNFTAddress = "0x3dE63D1da4639D24a65178ceb383A75003B71027";
+export const noxPlatform = "0x5730E5C8a37B6f24827809628022ADaF36139111";
