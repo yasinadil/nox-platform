525a149538e3949349cc1446979acbfe2ba3c6ae
yasinadil <adilyasin205@gmail.com>
2023-05-08 15:01:01 +0500

    loading v1.10

--- Config.ts
+++ Config.ts
@@ -1,4 +1,4 @@
 export const noxTokenAddress = "0x71411f2bbDAb65DfEC57BeC13C0E35A73E51d896";
 export const WETH_TokenAddress = "MATIC";
-export const noxNFTAddress = "0x43263dA028AB7248948C573c01fD0a6594Aef23C";
-export const noxPlatform = "0xE06f06E228EfcD0535e956a53056d42382353A58";
+export const noxNFTAddress = "0xed8BD3b44c773a507132cC727Ea0eEB76e1D84be";
+export const noxPlatform = "0x3D075c39D7C3f72f33ee5a0313F08ee4c6268D41";
